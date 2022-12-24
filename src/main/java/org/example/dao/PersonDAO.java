21c136bcd4c8c566c5f4fc87f400daaecc4bc0f9
Nataliya Kozlovskaya <natalija.kozlovskaya@gmail.com>
2022-12-25 00:43:52 +0300

    crud app: first_db, create new person

--- src/main/java/org/example/dao/PersonDAO.java
+++ src/main/java/org/example/dao/PersonDAO.java
@@ -3,42 +3,88 @@ package org.example.dao;
 import org.example.models.Person;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
-    private List<Person> people;
+    private static final String URL = "jdbc:mysql://localhost:3306/first_db";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+    private static Connection connection;
 
-    {
-        people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Mike", "lklk@.po", 15));
-        people.add(new Person(++PEOPLE_COUNT, "Tom", "mjihhgc@iu", 25));
-        people.add(new Person(++PEOPLE_COUNT, "Mari", "kijj@.kj", 98));
-        people.add(new Person(++PEOPLE_COUNT, "Tim", "mkmk@er", 78));
-
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public List<Person> index(){
+    public List<Person> index() {
+        List<Person> people = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(SQL);
+            while (resultSet.next()) {
+                Person person = new Person();
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setEmail(resultSet.getString("email"));
+                person.setAge(resultSet.getInt("age"));
+                people.add(person);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return people;
     }
 
-    public Person show(int id){
-        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    public Person show(int id) {
+//        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        return null;
     }
 
-    public void save(Person person){
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+    public void save(Person person) {
+//        person.setId(++PEOPLE_COUNT);
+//        people.add(person);
+        System.out.println(person);
+        try {
+
+//            String insert = "INSERT INTO Person(id, name, email, age) VALUES (?,?,?,?)";
+//            PreparedStatement ps = connection.prepareStatement(insert);
+//            ps.setInt(1,100);
+//            ps.setString(2,person.getName());
+//            ps.setString(3,person.getEmail());
+//            ps.setInt(4,person.getAge());
+//            ps.executeUpdate();
+
+            Statement statement = connection.createStatement();
+
+            String SQL = "INSERT INTO Person (id, name, email, age) VALUES(" + 1 + ",'" + person.getName() +
+                    "', '" + person.getEmail() + "','" + person.getAge() + "')";
+            statement.executeUpdate(SQL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-    public void update(int id, Person updatedPerson){
-        Person personToBeUpdated = show(updatedPerson.getId());
-        personToBeUpdated.setName(updatedPerson.getName());
-        personToBeUpdated.setAge(updatedPerson.getAge());
-        personToBeUpdated.setEmail(updatedPerson.getEmail());
+
+    public void update(int id, Person updatedPerson) {
+////        Person personToBeUpdated = show(updatedPerson.getId());
+//        personToBeUpdated.setName(updatedPerson.getName());
+//        personToBeUpdated.setAge(updatedPerson.getAge());
+//        personToBeUpdated.setEmail(updatedPerson.getEmail());
     }
     public void delete(int id){
-        people.removeIf(person -> person.getId() == id);
+//        people.removeIf(person -> person.getId() == id);
     }
 }
+
--- src/main/java/org/example/models/Person.java
+++ src/main/java/org/example/models/Person.java
@@ -57,4 +57,14 @@ public class Person {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
