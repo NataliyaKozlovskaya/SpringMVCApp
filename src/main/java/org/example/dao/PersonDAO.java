85b0cf6644a41f0267d03a6daf6c18cad5992ef1
Nataliya Kozlovskaya <natalija.kozlovskaya@gmail.com>
2023-01-06 00:49:11 +0300

    crud app: one_to_many, many_to_many, one_to_one, join

--- src/main/java/org/example/dao/PersonDAO.java
+++ src/main/java/org/example/dao/PersonDAO.java
@@ -31,7 +31,7 @@ public class PersonDAO {
     }
 
     public void save(Person person){
-        jdbcTemplate.update("INSERT INTO Person VALUES(?,?,?,?)", person.getId(), person.getName(), person.getEmail(), person.getAge());
+        jdbcTemplate.update("INSERT INTO Person(name, email, age) VALUES(?,?,?)", person.getName(), person.getEmail(), person.getAge());
     }
 
     public void update(int id, Person updatedPerson) {
