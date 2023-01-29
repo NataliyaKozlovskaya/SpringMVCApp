2ccdbff3c1c255f7a6669f213d71b0f6bb519d86
Nataliya Kozlovskaya <natalija.kozlovskaya@gmail.com>
2023-01-29 22:43:39 +0300

    crud app: project1 (hibernate, Spring JPA)??

--- src/main/java/org/example/controllers/BookController.java
+++ src/main/java/org/example/controllers/BookController.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
-import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -25,47 +25,74 @@ public class BookController {
         this.peopleService = peopleService;
     }
 
+    //    ПАГИНАЦИЯ
+//    @GetMapping("{page}&{bookPerPage}")
+//    public String getAllBooks(@RequestParam("page") Integer page, @RequestParam("bookPerPage") Integer bookPerPage,
+//                              Model model) {
+////        page=1;
+////        Integer defaultSize = 2;
+////        long pageCount = booksService.findCount()/defaultSize;
+//        long pageCount = booksService.findCount() / bookPerPage;
+//        model.addAttribute("pageCount", pageCount);
+//        model.addAttribute("page", booksService.findPage(page, bookPerPage));
+//        booksService.findPage();
+//        return "redirect:/books/getAllBooks";
+//    }
+//ПАГИНАЦИЯ
     @GetMapping
-    public String getAllBooks(Model model){
-        List<Book> allBooks = booksService.findAll();
-        model.addAttribute("allBooks", allBooks);
-        return "books/getAllBooks";
+    public String getAllBooks(@RequestParam(value="page", required = false) Integer page,
+                              @RequestParam(value="bookPerPage", required = false) Integer bookPerPage,
+                              Model model) {
+        if(page==null){
+            page=1;
+        }
+        if(bookPerPage==null){
+            bookPerPage= 1;
+        }
+        long countOfBooks = booksService.findCount();
+        long pageCount = countOfBooks / bookPerPage;
+        model.addAttribute("pageCount", pageCount);
+        model.addAttribute("page", booksService.findPage(page, bookPerPage));
+        return "redirect:/books/getAllBooks";
     }
+
+
     @GetMapping("/{id}")
-    public String getBookById(@PathVariable("id") Integer id, Model model, @ModelAttribute("person") Person person){
+    public String getBookById(@PathVariable("id") Integer id, Model model, @ModelAttribute("person") Person person) {
         Book book = booksService.findById(id);
         model.addAttribute("book", book);
 
         Optional<Person> owner = Optional.ofNullable(book.getPerson());
         if (owner.isPresent()) {
             model.addAttribute("owner", owner.get());
-        }else {
+        } else {
             model.addAttribute("peopleList", peopleService.findAll());
         }
         return "books/getBookById";
     }
 
+
     @PatchMapping("/{id}/addPerson")
-    public String setBookForPerson(@PathVariable("id") Integer id, @ModelAttribute("personId") Integer personId){
+    public String setBookForPerson(@PathVariable("id") Integer id, @ModelAttribute("personId") Integer personId) {
         booksService.setBookForPerson(id, personId);
         return "redirect:/books/{id}";
     }
 
     @PatchMapping("/{id}/free")
-    public String freeBook(@PathVariable("id") Integer id){
+    public String freeBook(@PathVariable("id") Integer id) {
         booksService.deletePersonFromBook(id);
         return "redirect:/books/{id}";
     }
 
     @GetMapping("/newBook")
-    public String newBook(Model model){
+    public String newBook(Model model) {
         model.addAttribute("book", new Book());
         return "books/newBook";
     }
 
     @PostMapping
     public String createNewBook(@ModelAttribute("book") @Valid Book book,
-                                BindingResult bindingResult){
+                                BindingResult bindingResult) {
         if (bindingResult.hasErrors())
             return "books/newBook";
         booksService.save(book);
@@ -74,7 +101,7 @@ public class BookController {
 
     @PatchMapping("/{id}")
     public String update(@PathVariable("id") Integer id, @ModelAttribute("book") @Valid Book book,
-                         BindingResult bindingResult){
+                         BindingResult bindingResult) {
         if (bindingResult.hasErrors())
             return "books/bookEdit";
         booksService.save(id, book);
@@ -82,14 +109,30 @@ public class BookController {
     }
 
     @GetMapping("/{id}/bookEdit")
-    public String edit(@PathVariable("id") Integer id, Model model){
+    public String edit(@PathVariable("id") Integer id, Model model) {
         model.addAttribute("book", booksService.findById(id));
         return "books/bookEdit";
     }
 
     @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") Integer id){
+    public String delete(@PathVariable("id") Integer id) {
         booksService.deleteById(id);
         return "redirect:/books";
     }
+
+
+    // ПОИСК КНИГИ
+//    @GetMapping("search")
+//    public String search(@RequestParam{"title"} String title, Model model){
+//        List<String> allTitle = booksService.findAllTitle();
+//        //как сравнить??
+//        List<String> books = allTitle.findByFirstnameLike(title);
+//        if (books.isEmpty()){
+//            model.addAttribute("allBooks", booksService.findAll());
+//         }else {
+//            model.addAttribute("books", books);
+//        }
+//
+//    }
+
 }
--- src/main/java/org/example/services/BooksService.java
+++ src/main/java/org/example/services/BooksService.java
@@ -5,6 +5,9 @@ import org.example.models.Person;
 import org.example.repositories.BooksRepository;
 import org.example.repositories.PeopleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,14 +25,16 @@ public class BooksService {
         this.peopleRepository = peopleRepository;
     }
 
-    public List<Book> findAll() {
+    public List<Book> findPage() {
         return booksRepository.findAll();
     }
+
     @Transactional
     public void save(Integer id, Book updateBook) {
         updateBook.setId(id);
         booksRepository.save(updateBook);
     }
+
     @Transactional
     public void save(Book book) {
         booksRepository.save(book);
@@ -43,12 +48,13 @@ public class BooksService {
     public List<Book> findAllByPersonId(Integer personId) {
         return booksRepository.findAllByPersonId(personId);
     }
+
     @Transactional
     public void setBookForPerson(Integer id, Integer personId) {
-       Book book = booksRepository.getOne(id);
-       Person person = peopleRepository.getOne(personId);
-       book.setPerson(person);
-       booksRepository.save(book);
+        Book book = booksRepository.getOne(id);
+        Person person = peopleRepository.getOne(personId);
+        book.setPerson(person);
+        booksRepository.save(book);
     }
 
     @Transactional
@@ -59,7 +65,27 @@ public class BooksService {
     }
 
     @Transactional
-    public void deleteById(Integer id){
+    public void deleteById(Integer id) {
         booksRepository.deleteById(id);
     }
+
+
+    //ПАГИНАЦИЯ
+    //    public Page<Book> findAll(Pageable pageable) {
+//        return null;
+    public Page<Book> findPage(Integer page, Integer bookPerPage){
+        Pageable pageable = PageRequest.of(page, bookPerPage);
+        return booksRepository.findAll(pageable);
+    }
+
+
+    // ПОИСК ВСЕХ НАЗВАНИЙ КНИГ
+//   /
+
+    //СЧИТАЕМ КОЛ-ВО СТРАНИЦ
+
+    public long findCount(){
+        return booksRepository.count();
+    }
+
 }
